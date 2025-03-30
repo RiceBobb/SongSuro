1029bfca86bb2a638d91a4c04ec4726567bd80c9
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-03-30 12:12:09 +0900

    finish making preprocess_f0 at melodyU

--- songsuro/encoder/melodyU.py
+++ songsuro/encoder/melodyU.py
@@ -12,17 +12,17 @@ def preprocess_f0(audio_filepath: str):
 	if not os.path.exists(audio_filepath):
 		raise FileNotFoundError(audio_filepath)
 
+	LEVELS = 127
 	MEL_MIN_VAL = 133
 	MEL_MAX_VAL = 571
 
 	f0_signal, fs = extract_f0_from_file(audio_filepath)
 	mel_f0 = hz_to_mel(f0_signal)
 	quantized_f0 = quantize_mel_scale(
-		mel_f0, levels=127, min_val=MEL_MIN_VAL, max_val=MEL_MAX_VAL
+		mel_f0, levels=LEVELS, min_val=MEL_MIN_VAL, max_val=MEL_MAX_VAL
 	)
 	frame_duration_ms = 20
 	indices_per_frame = int((frame_duration_ms / 1000) * fs)
 
-	_ = mode_window_filter(quantized_f0, indices_per_frame)
-
-	# TODO: cover non-voice frames (How to extract from YAPPT?) (Maybe by decibel?) Feature/#7
+	frame_quantized_f0 = mode_window_filter(quantized_f0, indices_per_frame)
+	return frame_quantized_f0
---
+++ tests/songsuro/encoder/test_melodyU.py
@@ -0,0 +1,25 @@
+import os
+import pathlib
+
+import numpy as np
+import pytest
+
+from songsuro.encoder.melodyU import preprocess_f0
+
+
+root_dir = pathlib.PurePath(os.path.dirname(os.path.realpath(__file__))).parent.parent
+resource_dir = os.path.join(root_dir, "resources")
+
+
+@pytest.fixture
+def sample_audio_file():
+	yield os.path.join(resource_dir, "sample_only_voice.wav")
+
+
+def test_mode_window_filter(sample_audio_file):
+	frame_quantized_f0 = preprocess_f0(sample_audio_file)
+
+	assert isinstance(frame_quantized_f0, np.ndarray)
+	assert frame_quantized_f0.ndim == 1
+	assert 0 in np.unique(frame_quantized_f0)
+	assert frame_quantized_f0.shape[0] < 2000
