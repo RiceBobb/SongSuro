b1421a8518ce425ea712bca5b5485254952a37bc
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-04-09 16:34:18 +0900

    Include prior at ConditionalEncoder

--- songsuro/condition/model.py
+++ songsuro/condition/model.py
@@ -2,13 +2,15 @@ from torch import nn
 
 from songsuro.condition.encoder.style import StyleEncoder
 from songsuro.condition.encoder.timbre import TimbreEncoder
+from songsuro.condition.prior_estimator import PriorEstimator
 
 
 class ConditionalEncoder(nn.Module):
-	def __init__(self, hidden_size: int):
+	def __init__(self, hidden_size: int, prior_output_dim: int):
 		super().__init__()
 		self.timbre_encoder = TimbreEncoder(hidden_size, vq_input_dim=80)
 		self.style_encoder = StyleEncoder(hidden_size)
+		self.prior_estimator = PriorEstimator(hidden_size, prior_output_dim)
 
 	def forward(self, lyrics, original_audio):
 		"""
@@ -22,4 +24,6 @@ class ConditionalEncoder(nn.Module):
 		timbre_embedding = self.timbre_encoder(lyrics)
 		style_embedding = self.style_encoder(lyrics)
 
-		return style_embedding + timbre_embedding
+		prior = self.prior_estimator(style_embedding + timbre_embedding)
+
+		return style_embedding + timbre_embedding, prior
