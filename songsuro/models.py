37ffc4b89afa992af00b85e2da86b61d66e05087
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-04-09 19:11:49 +0900

    temporal train.py and new Songsuro models.py

---
+++ songsuro/models.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+
+class Songsuro(nn.Module):
+	def __init__(self):
+		super().__init__()
+
+	def forward(self):
+		"""
+		Forward process (in training) of the Songsuro model.
+
+		:return:
+		"""
+		pass
+
+	def sample(self):
+		"""
+		Inference of the Songsuro model.
+
+		:return: The generated waveform.
+		"""
+		pass
