51be6509fbf9e6a98ebecd6e4524732b5b5db248
minsing-jin <ironman0722@naver.com>
2025-05-02 18:34:10 +0900

    FFTEncoder Implementation complete.

--- songsuro/condition/encoder/fft.py
+++ songsuro/condition/encoder/fft.py
@@ -6,63 +6,73 @@ Relative position encoder github: https://github.com/evelinehong/Transformer_Rel
 FastSpeech2 transformer github: https://github.com/ming024/FastSpeech2/blob/master/transformer/
 """
 
-import torch.nn as nn
-from songsuro.modules.fft.relative_pos_encoding import RelativePosition
-from songsuro.modules.fft.layers import FFTBlock
+import math
+import torch
+from torch import nn
+
+import attentions
 
 
-# TODO: Use config yaml file later
-class FFTransformerBlock(nn.Module):
-	def __init__(self, d_model=192, d_k=64, d_v=64, n_head=2, d_inner=768):
+class FFTEncoder(nn.Module):
+	def __init__(
+		self,
+		input_vec_size,
+		out_channels,
+		hidden_channels,
+		filter_channels,
+		filter_channels_dp,
+		n_heads,
+		n_layers,
+		kernel_size,
+		p_dropout,
+		window_size=None,
+		block_length=None,
+		mean_only=False,
+		gin_channels=0,
+	):
 		super().__init__()
-		max_relative_position = 4
 
-		self.ff_block = FFTBlock(
-			hidden_size_d_model=d_model,
-			d_k=d_k,
-			d_v=d_v,
-			d_inner=d_inner,
-			n_head=n_head,
-			kernel_size=9,
+		self.input_vec_size = input_vec_size  # number of characters
+		self.out_channels = out_channels
+		self.hidden_channels = hidden_channels
+		self.filter_channels = filter_channels
+		self.filter_channels_dp = filter_channels_dp
+		self.n_heads = n_heads
+		self.n_layers = n_layers
+		self.kernel_size = kernel_size
+		self.p_dropout = p_dropout
+		self.window_size = window_size
+		self.block_length = block_length
+		self.mean_only = mean_only
+		self.gin_channels = gin_channels
+
+		self.emb = nn.Embedding(input_vec_size, hidden_channels)
+		nn.init.normal_(self.emb.weight, 0.0, hidden_channels**-0.5)
+
+		self.encoder = attentions.Encoder(
+			hidden_channels,
+			filter_channels,
+			n_heads,
+			n_layers,
+			kernel_size,
+			p_dropout,
+			window_size=window_size,
+			block_length=block_length,
 		)
-		self.relative_position = RelativePosition(d_model, max_relative_position)
 
-	def forward(self, x, mask=None):
-		if mask is not None:
-			mask = mask.unsqueeze(1).unsqueeze(2)
-		x, _ = self.ff_block(x, mask=mask)
+	def forward(self, x, x_lengths):
+		x = self.emb(x) * math.sqrt(self.hidden_channels)  # [b, t, h]
+		x = torch.transpose(x, 1, -1)  # [batch, hidden, time(seq_len)]
+		# mask for padding
+		x_mask = torch.unsqueeze(self.sequence_mask(x_lengths, x.size(2)), 1).to(
+			x.dtype
+		)
+
+		x = self.encoder(x, x_mask)
 		return x
 
-
-class LyricsEncoder(nn.Module):
-	"""
-	Block is 4 in HiddenSinger. - n_layers = 4
-	"""
-
-	def __init__(self, vocab_size, d_model=192, n_layers=4):
-		super().__init__()
-		self.embed = nn.Embedding(vocab_size, d_model)
-		self.blocks = nn.ModuleList(
-			[FFTransformerBlock(d_model=d_model) for _ in range(n_layers)]
-		)
-		self.prior = nn.Linear(d_model, d_model)
-
-	def forward(self, x, mask=None):
-		x = self.embed(x)
-		for block in self.blocks:
-			x = block(x, mask)
-		return self.prior(x)
-
-
-class EnhancedConditionEncoder(nn.Module):
-	def __init__(self, d_model=192, n_layers=4):
-		super().__init__()
-		self.blocks = nn.ModuleList(
-			[FFTransformerBlock(d_model=d_model) for _ in range(n_layers)]
-		)
-		self.prior = nn.Linear(d_model, d_model)
-
-	def forward(self, x, mask=None):
-		for block in self.blocks:
-			x = block(x, mask)
-		return self.prior(x)
+	def sequence_mask(self, length, max_length=None):
+		if max_length is None:
+			max_length = length.max()
+		x = torch.arange(max_length, dtype=length.dtype, device=length.device)
+		return x.unsqueeze(0) < length.unsqueeze(1)
