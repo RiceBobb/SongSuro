72eb73b49c4452bd04c5d5f1e0c842070e99c708
kimbwook <bwook00@naver.com>
2025-05-08 16:59:37 +0900

    delete useless get_logger function

--- songsuro/autoencoder/train.py
+++ songsuro/autoencoder/train.py
@@ -488,21 +488,6 @@ def latest_checkpoint_path(dir_path, regex="G_*.pth"):
 	return x
 
 
-def get_logger(model_dir, filename="train.log"):
-	global logger
-	logger = logging.getLogger(os.path.basename(model_dir))
-	logger.setLevel(logging.DEBUG)
-
-	formatter = logging.Formatter("%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
-	if not os.path.exists(model_dir):
-		os.makedirs(model_dir)
-	h = logging.FileHandler(os.path.join(model_dir, filename))
-	h.setLevel(logging.DEBUG)
-	h.setFormatter(formatter)
-	logger.addHandler(h)
-	return logger
-
-
 def count_parameters(model):
 	return sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6
 
