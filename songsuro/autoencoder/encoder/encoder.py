a27df00f27fc2cb48fd45b9e11ee1bff9f1a0cac
Bwook (Byoungwook) Kim <bwook00@naver.com>
2025-05-13 20:13:20 +0900

    Edit Encoder's padding logic (#59)

--- songsuro/autoencoder/encoder/encoder.py
+++ songsuro/autoencoder/encoder/encoder.py
@@ -25,8 +25,24 @@ class ConvReLURes(nn.Module):
 		super().__init__()
 		self.n_in = n_in_chan
 		self.n_out = n_out_chan
+		# 필터 크기와 스트라이드에 따른 패딩 계산
+		if stride == 1:
+			# 입력과 출력 크기를 동일하게 유지하기 위한 패딩
+			if filter_sz % 2 == 0:  # 짝수 필터 크기인 경우 비대칭 패딩 필요
+				self.asymmetric_padding = True
+				self.pad_left = filter_sz // 2 - 1
+				self.pad_right = filter_sz // 2
+				padding = 0  # Conv1d에는 패딩 없이 수동으로 적용
+			else:
+				self.asymmetric_padding = False
+				padding = (filter_sz - 1) // 2
+		else:
+			# 스트라이드가 1보다 큰 경우 다운샘플링 발생
+			padding = (filter_sz - 1) // 2
+			self.asymmetric_padding = False
+
 		self.conv = nn.Conv1d(
-			n_in_chan, n_out_chan, filter_sz, stride, padding=0, bias=True
+			n_in_chan, n_out_chan, filter_sz, stride, padding=padding, bias=True
 		)
 		self.relu = nn.ReLU()
 		self.name = name
@@ -52,19 +68,33 @@ class ConvReLURes(nn.Module):
 		B, C, T = n_batch, n_in_chan, n_win
 		x: (B, C, T)
 		"""
+		identity = x.clone()  # 원본 입력 저장
+
+		if self.asymmetric_padding:
+			x = nn.functional.pad(x, (self.pad_left, self.pad_right))
+
 		pre = self.conv(x)
-		# out = self.bn(out)
 		act = self.relu(pre)
+
 		if self.do_res:
-			act[...] += x[
-				:, :, self.residual_offsets[0] : self.residual_offsets[1] or None
-			]
-			# act += x[:,:,self.residual_offsets[0]:self.residual_offsets[1] or None]
-		# act_sum = act.sum()
+			# 입력과 출력의 크기 비교
+			if act.shape[2] != identity.shape[2]:
+				# 크기가 다른 경우 중앙 부분 잘라내기
+				if act.shape[2] < identity.shape[2]:
+					# 출력이 더 작은 경우 입력 잘라내기
+					diff = identity.shape[2] - act.shape[2]
+					start = diff // 2
+					identity = identity[:, :, start : start + act.shape[2]]
+				else:
+					# 입력이 더 작은 경우 출력 잘라내기
+					diff = act.shape[2] - identity.shape[2]
+					start = diff // 2
+					act = act[:, :, start : start + identity.shape[2]]
+
+			# 이제 크기가 같으므로 더하기
+			act = act + identity
+
 		self.frac_zero_act = (act == 0.0).sum().double() / act.nelement()
-		# if act_sum.eq(0.0):
-		#    print('encoder layer {}: {}'.format(self.name, act_sum))
-		# print('bias mean: {}'.format(self.conv.bias.mean()))
 		return act
 
 
