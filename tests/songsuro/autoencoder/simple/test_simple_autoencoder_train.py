38ab2f0f24d1875a2fdeb2db35747d0ecc6f36e1
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-05-19 19:52:42 +0900

    profiling try in cuda in ssh remote

--- tests/songsuro/autoencoder/simple/test_simple_autoencoder_train.py
+++ tests/songsuro/autoencoder/simple/test_simple_autoencoder_train.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-
-from songsuro.autoencoder.simple.train import train
+from songsuro.autoencoder.train import train
 
 
 # @pytest.mark.skip
@@ -10,3 +9,7 @@ def test_autoencoder_train():
 	data_dir = root_dir / "tests" / "resources" / "ai_hub_data_sample"
 	checkpoint_dir = root_dir / "simple_autoencoder_checkpoint"
 	train(str(data_dir), str(data_dir), 4, 6, checkpoint_dir)
+
+
+if __name__ == "__main__":
+	test_autoencoder_train()
